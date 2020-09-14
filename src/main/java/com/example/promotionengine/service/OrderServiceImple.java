51ed91f8da3dda0eaea34bafbfbeea966606b23d
Shubhasis Bhattacharya <shiningshubha@gmail.com>
2020-09-14 11:19:32 +0530

    Implementation added

--- src/main/java/com/example/promotionengine/service/OrderServiceImple.java
+++ src/main/java/com/example/promotionengine/service/OrderServiceImple.java
@@ -1,4 +1,13 @@
 package com.example.promotionengine.service;
 
-public class OrderServiceImple {
+import com.example.promotionengine.domain.Promotion;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OrderServiceImple implements IPromoService {
+    @Override
+    public Optional<Promotion> findPromoByProdId(String prod_id, List<Promotion> promotionList) {
+        return Optional.empty();
+    }
 }
