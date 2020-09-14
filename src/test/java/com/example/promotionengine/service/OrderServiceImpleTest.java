c6cd78952941db6f2fae36c4080ddce5290e06e4
Shubhasis Bhattacharya <shiningshubha@gmail.com>
2020-09-14 11:58:55 +0530

    Final upload with 2 tesct cases

--- src/test/java/com/example/promotionengine/service/OrderServiceImpleTest.java
+++ src/test/java/com/example/promotionengine/service/OrderServiceImpleTest.java
@@ -59,8 +59,6 @@ public class OrderServiceImpleTest {
         products2.add(new Product("A", 50.0, 5));
         products2.add(new Product("B", 30.0, 5));
         products2.add(new Product("c", 20.0, 1));
-        Order order2 = new Order(2, products2);
- //Mockito.when(promoServiceImpl.findPromoByProdId("A", promotions)).thenReturn(Optional.of(promotions.get(1)));
         Double price = iOrderService.getTotalPrice(products2, promotions);
         Assertions.assertEquals(370, price);
     }
