bf34894eb2b7e7f12db042a920aa90279af39f25
Shubhasis Bhattacharya <shiningshubha@gmail.com>
2020-09-14 11:49:47 +0530

    Test commit started 4, added setup

--- src/test/java/com/example/promotionengine/service/OrderServiceImpleTest.java
+++ src/test/java/com/example/promotionengine/service/OrderServiceImpleTest.java
@@ -1,11 +1,14 @@
 package com.example.promotionengine.service;
 
 import com.example.promotionengine.domain.Promotion;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -17,6 +20,25 @@ public class OrderServiceImpleTest {
     PromoServiceImple promoServiceImpl;
 
     private List<Promotion> promotions;
+
+    @Before
+    public void setup() {
+        HashMap<String, Integer> promo_product_map1 = new HashMap<String, Integer>();
+        promo_product_map1.put("A", 3);
+
+        HashMap<String, Integer> promo_product_map2 = new HashMap<String, Integer>();
+        promo_product_map2.put("B", 2);
+
+        HashMap<String, Integer> promo_product_map3 = new HashMap<String, Integer>();
+        promo_product_map3.put("C", 1);
+        promo_product_map3.put("D", 1);
+
+        promotions = new ArrayList<Promotion>();
+        promotions.add(new Promotion(1, 130.0, "Fixed Price", promo_product_map1));
+        promotions.add(new Promotion(2, 45.0, "Fixed Price", promo_product_map2));
+        promotions.add(new Promotion(3, 30.0, "Fixed Price", promo_product_map3));
+
+    }
 
 
 }
