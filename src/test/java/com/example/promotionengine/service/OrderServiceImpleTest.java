802ec7ef6bd1e2ad61d8530aefafdd7327621104
Shubhasis Bhattacharya <shiningshubha@gmail.com>
2020-09-14 11:41:39 +0530

    Test commit started 3

--- src/test/java/com/example/promotionengine/service/OrderServiceImpleTest.java
+++ src/test/java/com/example/promotionengine/service/OrderServiceImpleTest.java
@@ -1,8 +1,22 @@
 package com.example.promotionengine.service;
 
+import com.example.promotionengine.domain.Promotion;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 public class OrderServiceImpleTest {
+
+    @InjectMocks
+    IOrderService iOrderService = new OrderServiceImple();
+    @Mock
+    PromoServiceImple promoServiceImpl;
+
+    private List<Promotion> promotions;
+
+
 }
