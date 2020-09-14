ed97f1377bd2bfabcae3042711e2cc3dddeab6cc
Shubhasis Bhattacharya <shiningshubha@gmail.com>
2020-09-14 11:18:37 +0530

    Promo order  service added

--- src/main/java/com/example/promotionengine/service/PromoServiceImple.java
+++ src/main/java/com/example/promotionengine/service/PromoServiceImple.java
@@ -6,7 +6,7 @@ import com.example.promotionengine.domain.Promotion;
 import java.util.List;
 import java.util.Optional;
 
-public class PromoService implements IPromoService {
+public class PromoServiceImple implements IPromoService {
 
 
     public Optional<Promotion> findPromoByProdId(String prod_id, List<Promotion> promotionList) {
